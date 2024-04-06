dde37170262199b51483029b614d6aaa7a5dd753
Maxime <maxpolizzo@gmail.com>
2024-04-06 22:46:12 +0200

    CHANGED: camera component style and buttons

--- lnbits/extensions/monopoly/static/js/helpers/camera.js
+++ lnbits/extensions/monopoly/static/js/helpers/camera.js
@@ -3,14 +3,14 @@ export function paintOutline(detectedCodes, ctx) {
   console.log(detectedCodes)
   console.log(ctx)
 
+
   if(detectedCodes && detectedCodes.length) {
     for (const detectedCode of detectedCodes) {
       console.log(detectedCode)
 
       const [firstPoint, ...otherPoints] = detectedCode.cornerPoints
 
-      ctx.strokeStyle = '#007bff'
-
+      ctx.strokeStyle = 'orange'
       ctx.beginPath()
       ctx.moveTo(firstPoint.x, firstPoint.y)
       for (const { x, y } of otherPoints) {
@@ -23,15 +23,40 @@ export function paintOutline(detectedCodes, ctx) {
   }
 }
 
-// QR code track function (bounding box)
-function paintBoundingBox(detectedCodes, ctx) {
-  for (const detectedCode of detectedCodes) {
-    const {
-      boundingBox: { x, y, width, height }
-    } = detectedCode
+export function paintCenterText(detectedCodes, ctx) {
+  if(detectedCodes && detectedCodes.length) {
+    for (const detectedCode of detectedCodes) {
+      const { boundingBox, rawValue } = detectedCode
 
-    ctx.lineWidth = 2
-    ctx.strokeStyle = '#007bff'
-    ctx.strokeRect(x, y, width, height)
+      const centerX = boundingBox.x + boundingBox.width / 2
+      const centerY = boundingBox.y + boundingBox.height / 2
+
+      const fontSize = Math.max(12, (50 * boundingBox.width) / ctx.canvas.width)
+
+      ctx.font = `bold ${fontSize}px sans-serif`
+      ctx.textAlign = 'center'
+
+      ctx.lineWidth = 3
+      ctx.strokeStyle = '#35495e'
+      ctx.strokeText(detectedCode.rawValue, centerX, centerY)
+
+      ctx.fillStyle = '#5cb984'
+      ctx.fillText(rawValue, centerX, centerY)
+    }
+  }
+}
+
+// QR code track function (bounding box)
+export function paintBoundingBox(detectedCodes, ctx) {
+  if(detectedCodes && detectedCodes.length) {
+    for (const detectedCode of detectedCodes) {
+      const {
+        boundingBox: { x, y, width, height }
+      } = detectedCode
+
+      ctx.lineWidth = 2
+      ctx.strokeStyle = '#007bff'
+      ctx.strokeRect(x, y, width, height)
+    }
   }
 }
