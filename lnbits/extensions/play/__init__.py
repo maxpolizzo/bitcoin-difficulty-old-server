93774a3204de700c8ff439b737bf1224c4c8218f
Maxime <maxpolizzo@gmail.com>
2024-06-15 13:33:03 +0200

    FIXED: event loop for outgoing payments checker

--- lnbits/extensions/play/__init__.py
+++ lnbits/extensions/play/__init__.py
@@ -5,7 +5,6 @@ from fastapi.staticfiles import StaticFiles
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
-from lnbits.tasks import catch_everything_and_restart
 
 from .tasks import PaymentsWatcher
 from .websocket import WebsocketManager
@@ -32,7 +31,4 @@ from .views_api import *  # noqa
 
 def play_start(): # Executed in lnbits app.py for all valid extensions
     paymentsWatcher = PaymentsWatcher(db, websocketManager)
-    loop = asyncio.get_event_loop()
-    loop.create_task(catch_everything_and_restart(paymentsWatcher.wait_for_paid_invoices))
-    loop.create_task(catch_everything_and_restart(paymentsWatcher.wait_for_outgoing_payments))
-
+    paymentsWatcher.create_tasks()
